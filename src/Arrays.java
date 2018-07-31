0022ea650a9eeb484249b912bdc232ea6ab5575d
MKovar <mkovar@mkovar.local>
2018-07-31 16:30:05 -0500

    java array lec

---
+++ src/Arrays.java
@@ -0,0 +1,50 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+//        String[] cities = new String[3];
+//        cities[0] = "San Antonio";
+//        cities[1] = "Dallas";
+//        cities[2] = "Houston";
+//
+//        System.out.println(cities[0]);
+////        SA
+
+//        cities refactored to one statement:
+//        String[] cities = {"San Antonio", "Dallas", "Houston"};
+//        System.out.println(cities[0]);
+//        SA
+
+
+//        int[] numbers = new int[3];
+//        numbers[0] = 1;
+
+// default value depends on data type. If we tried to access int[1] or int[2],
+// we would see 0 as its default instead of "null".
+
+//        final int NUMBER_OF_BEATLES = 4;
+//        BandMember[] theBeatles = new BandMember[NUMBER_OF_BEATLES];
+//        theBeatles[0] = new BandMember("John");
+//        theBeatles[1] = new BandMember("Paul");
+//        theBeatles[2] = new BandMember("George");
+//        theBeatles[3] = new BandMember("Ringo");
+
+//        System.out.println(theBeatles[2].getName());
+//        George
+
+
+//        for loop   \\
+
+//        String[] languages = {"html", "css", "javascript", "java"};
+//        for (int i = 0; i < languages.length; i++){
+//            System.out.println(languages[i]);
+//        }
+
+//        enhanced for loop AKA forEach loop // doesnt come with index \\
+//        for (String language : languages) {
+//            System.out.println(language);
+//        }
+
+
+    }
+}
