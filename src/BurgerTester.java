8c588091f6c57d6a1ceb83f9c5d2744587e830a3
MKovar <mkovar@mkovar.local>
2018-07-31 16:28:35 -0500

    exercise of working with classes and methods of classes

---
+++ src/BurgerTester.java
@@ -0,0 +1,21 @@
+class BurgerTester {
+
+    public static void main(String[] args) {
+
+//        public String mostPopularTopping;
+//        public int averageDaysBeforeExperiation;
+//        public int temperatureWhenCooked;
+//
+//        public void  grill(){
+//            System.out.println("Grilling burger!");
+//        }
+
+
+        System.out.println(BurgerTools.mostPopularTopping);
+        System.out.println(BurgerTools.averageDaysBeforeExperiation);
+        System.out.println(BurgerTools.temperatureWhenCooked);
+        BurgerTools.grill();
+
+
+    }
+}
---
+++ src/BurgerTools.java
@@ -0,0 +1,11 @@
+public class BurgerTools {
+
+    public static String mostPopularTopping = "cheese";
+    public static int averageDaysBeforeExperiation = 5;
+    public static int temperatureWhenCooked = 300;
+
+    public static void grill(){
+        System.out.println("Grilling burger!");
+    }
+
+}
